385a44dbec95f15f7612864c1d48b8bee5835d3f
Artemy Khanov <hanov_artem@idpowers.com>
2020-08-30 18:48:20 +0400

    lesson 2 homework

---
+++ src/data/texts.js
@@ -0,0 +1,2 @@
+export const text1 = 'Первое сообщение';
+export const text2 = 'Второе сообщение';
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import showAlert from '@/helpers';
+import { text1, text2 } from '@/data/texts';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
@@ -6,3 +8,6 @@ Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+showAlert(text1);
+showAlert(text2);
