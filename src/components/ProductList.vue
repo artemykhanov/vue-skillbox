b0427a0020f69827e96f375b17d3e92e94e2bb2b
Artemy Khanov <hanov_artem@idpowers.com>
2020-08-30 18:36:48 +0400

    add ProductList, ProductItem components

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem :product="product" :key="index"
+                 v-for="(product, index) in products"></ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  props: ['products'],
+  components: { ProductItem },
+};
+</script>
---
+++ src/data/products.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: 'Значок Ван Гог',
+    price: 500,
+    image: 'img/znachok_van_gog.jpg',
+  },
+  {
+    title: 'Футболка с автобусом',
+    price: 1299,
+    image: 'img/tshirt.png',
+  },
+  {
+    title: 'Фигурка Рука',
+    price: 760,
+    image: 'img/hand.jpg',
+  },
+  {
+    title: 'Футболка Кусь',
+    price: 1500,
+    image: 'img/kus.jpg',
+  },
+  {
+    title: 'Пиво Seaman',
+    price: 120,
+    image: 'img/beer.png',
+  },
+  {
+    title: 'Блендер погружной',
+    price: 4699,
+    image: 'img/blender.jpg',
+  },
+  {
+    title: 'Бойня №5. Курт Воннегут',
+    price: 346,
+    image: 'img/book-5.jpg',
+  },
+  {
+    title: 'Фигурка декоративная',
+    price: 455,
+    image: 'img/figure.jpg',
+  },
+  {
+    title: 'Playstation 5',
+    price: 49999,
+    image: 'img/playstation_5.jpg',
+  },
+  {
+    title: 'Настенное украшение',
+    price: 399,
+    image: 'img/wall-decoration.jpg',
+  },
+];
